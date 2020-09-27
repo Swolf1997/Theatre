e8af3d06d02fb3b53b0bace93ff50b7d353b9c08
Swolf1997 <71954090+Swolf1997@users.noreply.github.com>
2020-09-27 17:00:04 -0500

    Add files via upload

--- Theatre.java
+++ Theatre.java
@@ -1,14 +1,30 @@
-package sem2project1;
+package project_1;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import project_1.Movies.Status;
+
 
 public class Theatre {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 	
 	//Create linked list
-		//LinkedList list = new LinkedList();
+	Deque<Movies> list = new LinkedList<Movies>();
+	// Create iterator
+	Iterator<Movies> it = list.iterator();
+	// Data fields of {Movies}
+	Date releaseDate;
+	String name;
+	String desc;
+	Date receivedDate;
+	Status status;
+	// For formating dates
+	SimpleDateFormat sdfrmt = new SimpleDateFormat("MM/dd/yyyy");
+	sdfrmt.setLenient(false);
 		
 	//Create scanner
 	Scanner scanner = new Scanner(System.in);
@@ -25,35 +41,63 @@ public class Theatre {
 	System.out.println("Press 'E' to edit movies.");
 	System.out.println("Press 'C' to count movies.");
 	System.out.println("Press 'X' to terminate program.");
+	String option = scanner.next();
 	
 	
-	While true {
-		String option = scanner.next();
-		if (option.equals("D")) {
+	// Boolean for while loop
+	Boolean run = true;
+	while (run) {
+		if (option.toUpperCase().equals("D")) {
 			System.out.println("You have selected to display movies.");
 			//Code to display movies
-		} else if (option.equals("A")) {
+		} else if (option.toUpperCase().equals("A")) {
 			System.out.println("You have selected to add a movie.");
-			//Code to add movie 
-		} else if (option.equals("S")) {
+			// Getting the info for the movie
+			String[] movieInfo = reader.nextLine().split(", ");
+			// Storing the info
+			name = movieInfo[0];
+			// Checking if the dates are correctly formated.
+			try {
+				releaseDate = sdfrmt.parse(movieInfo[1]);
+				receivedDate = sdfrmt.parse(movieInfo[3]);
+			}
+			catch (Exception e) {
+				System.out.println("Invalid date format");
+				continue;
+			}
+			desc = movieInfo[2];
+			status = Status.valueOf(movieInfo[4].toUpperCase());
+			// Adding to {list}
+			list.addLast(new Movies(releaseDate, name, desc, receivedDate, status));
+		} else if (option.toUpperCase().equals("S")) {
 			System.out.println("You have selected to show movies with a given release date.");
 			//Code to show movies with given release date
-		} else if (option.equals("E")) {
+		} else if (option.toUpperCase().equals("E")) {
 			System.out.println("You have selected to edit movies.");
 			//Code to edit movies
-		} else if (option .equals("C")) {
+		} else if (option.toUpperCase().equals("C")) {
 			System.out.println("You have selected to count movies.");
 			//Code to count movies
-		} else if (option.equals("X")) {
+		} else if (option.toUpperCase().equals("X")) {
 			System.out.println("You have selected to terminate the program.");
-			//Code to Terminate program
+			run = false;
 			break;
 		} else {
 			System.out.println("You have selected an invalid option, please try again.");
 			continue;
+		}
+		System.out.println("\nPlease choose an option: ");
+		System.out.println("Press 'D' to display movies.");
+		System.out.println("Press 'A' to add a movie.");
+		System.out.println("Press 'S' to show movies with a given release date.");
+		System.out.println("Press 'E' to edit movies.");
+		System.out.println("Press 'C' to count movies.");
+		System.out.println("Press 'X' to terminate program.");
+		option = scanner.next();
 	}
 	
 	//Close Everything
+	reader.close();
 	scanner.close();
 	inputFile.close();
 	
